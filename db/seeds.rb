9c626c70a1b7a54cab0a67218d3c91ed0d0797b0
ruthno <ruthno@gmail.com>
2015-03-03 22:42:32 -0500

    Initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,12 @@ Event.create(name: "Pottery Painting", from: DateTime.parse('14th Jan 2015 06:00
 d5 = Day.create(date: DateTime.parse('15th Jan 2015 00:00 AM'))
 
 d6 = Day.create(date: DateTime.parse('16th Jan 2015 00:00 AM'))
-Event.create(name: "Casual Friday", from: DateTime.parse('16th Jan 2015 00:00 AM'), to: DateTime.parse('17th Jan 2015 00:00 AM'), location: "", day_id: d6.id)
+Event.create(name: "Casual Friday", from: DateTime.parse('16th Jan 2015 03:00 PM'), to: DateTime.parse('17th Jan 2015 05:00 PM'), location: "", day_id: d6.id)
 Event.create(name: "Running", from: DateTime.parse('16th Jan 2015 08:00 PM'), to: DateTime.parse('16th Jan 2015 08:30 PM'), location: "", day_id: d6.id)
 Event.create(name: "Grocery Shopping", from: DateTime.parse('16th Jan 2015 09:00 PM'), to: DateTime.parse('16th Jan 2015 09:30 AM'), location: "", day_id: d6.id)
 
-d6 = Day.create(date: DateTime.parse('17th Jan 2015 00:00 AM'))
+d7 = Day.create(date: DateTime.parse('17th Jan 2015 00:00 AM'))
+
+d8 = Day.create(date: DateTime.parse('18th Jan 2015 00:00 AM'))
+Event.create(name: "Happy Hour", from: DateTime.parse('18th Jan 2015 10:00 PM'), to: DateTime.parse('19th Jan 2015 12:00 AM'), location: "Local 61, Brooklyn, NY", day_id: d8.id)
+Event.create(name: "Dinner Party", from: DateTime.parse('18th Jan 2015 06:30 PM'), to: DateTime.parse('18th Jan 2015 09:30PM'), location: "125 Court Street, Brooklyn NY", day_id: d8.id)
